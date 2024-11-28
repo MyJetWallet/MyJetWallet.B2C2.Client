cbbc0ca7ff59222a18c7f1b11a97d22b6f730b0a
Alexey Novichikhin <alexey.n@smplt.net>
2024-11-28 14:57:49 +0200

    add datacontract to UnsecureLoan model

--- src/MyJetWallet.B2C2.Client/Models/Rest/UnsecureLoan.cs
+++ src/MyJetWallet.B2C2.Client/Models/Rest/UnsecureLoan.cs
@@ -1,16 +1,18 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace MyJetWallet.B2C2.Client.Models.Rest;
 
+[DataContract]
 public class UnsecureLoan
 {
-    public string trade_id { get; set; }
-    public string currency { get; set; }
-    public string side { get; set; }
-    public string notional { get; set; }
-    public string rate { get; set; }
-    public DateTime? created { get; set; }
-    public DateTime? start_date { get; set; }
-    public DateTime? end_date { get; set; }
+    [DataMember(Order = 1)] public string trade_id { get; set; }
+    [DataMember(Order = 2)] public string currency { get; set; }
+    [DataMember(Order = 3)] public string side { get; set; }
+    [DataMember(Order = 4)] public string notional { get; set; }
+    [DataMember(Order = 5)] public string rate { get; set; }
+    [DataMember(Order = 6)] public DateTime? created { get; set; }
+    [DataMember(Order = 7)] public DateTime? start_date { get; set; }
+    [DataMember(Order = 8)] public DateTime? end_date { get; set; }
     // etd...
 }
